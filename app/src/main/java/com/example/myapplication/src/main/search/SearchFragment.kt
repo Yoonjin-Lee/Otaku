19e45d440b37346112b747d4b5cfeca38c0f1c87
Yoonjin-Lee <peacemiller@konkuk.ac.kr>
2023-09-12 22:43:31 +0900

    create : resultActivity - searchview

--- app/src/main/java/com/example/myapplication/src/main/search/SearchFragment.kt
+++ app/src/main/java/com/example/myapplication/src/main/search/SearchFragment.kt
@@ -3,14 +3,29 @@ package com.example.myapplication.src.main.search
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.widget.SearchView
 import com.example.myapplication.R
 import com.example.myapplication.config.BaseFragment
 import com.example.myapplication.databinding.FragmentSearchBinding
 import com.example.myapplication.src.main.search.category.CategoryActivity
+import com.example.myapplication.src.main.search.result.ResultActivity
 
 class SearchFragment : BaseFragment<FragmentSearchBinding>(FragmentSearchBinding::bind, R.layout.fragment_search) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.searchSearch.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                val intent = Intent(context, ResultActivity::class.java)
+                intent.putExtra("query", query)
+                startActivity(intent)
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
+            }
+        })
 
         binding.searchBtnIdol.setOnClickListener {
             val intent = Intent(context, CategoryActivity::class.java)
