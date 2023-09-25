b8a0a5bfaeaafc8c149f6515a09188b8493519d1
Yoonjin <yoonjin@Yoonjinui-MacBookAir.local>
2023-09-25 23:01:54 +0900

    create : DonationRVAdapter's setdata func, need : ItemClickListener error fixing

--- app/src/main/java/com/example/myapplication/src/main/mypage/manage/manageDonation/DonationDialog.kt
+++ app/src/main/java/com/example/myapplication/src/main/mypage/manage/manageDonation/DonationDialog.kt
@@ -3,7 +3,7 @@ package com.example.myapplication.src.main.mypage.manage.manageDonation
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.util.Log
 import com.example.myapplication.config.BaseActivity
 import com.example.myapplication.databinding.DialogDonationBinding
 
@@ -13,10 +13,15 @@ class DonationDialog(
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        supportActionBar?.hide()
+
+        val position = intent.getIntExtra("position", -1)
+        Log.d("dialog_position", "${position}")
+
         window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
 
         binding.logoutYes.setOnClickListener {
+            itemClickListener?.onClick(position)
+            Log.d("dialog_position2", "${position}")
             this.finish()
         }
 
@@ -24,4 +29,14 @@ class DonationDialog(
             this.finish()
         }
     }
+
+    interface OnItemClickListener{
+        fun onClick(position : Int)
+    }
+
+    fun setItemClickListener(onItemClickListener: OnItemClickListener){
+        this.itemClickListener = onItemClickListener
+    }
+
+    private var itemClickListener: OnItemClickListener? = null
 }
